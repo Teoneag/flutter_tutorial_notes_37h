92caec73c09019d31370b81caffc6684402b28cd
Teodor Neagoe <teodor.neagoe.climber@gmail.com>
2023-04-14 18:43:37 +0300

    step-10

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -7,9 +7,8 @@ class AuthService implements AuthProvider {
   final AuthProvider provider;
   const AuthService(this.provider);
 
-  factory AuthService.firebase() => AuthService(
-        FirebaseAuthProvider(),
-      );
+  factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
+
   @override
   Future<AuthUser> createUser({
     required String email,
